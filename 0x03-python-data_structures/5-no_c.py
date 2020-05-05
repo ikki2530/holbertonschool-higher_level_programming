c39e779fae76a6f992f84c949baeae9b5f6769de
Diego Gómez <dagomez2530@gmail.com>
2020-05-05 20:21:11 +0000

    Fixing checks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     if my_string:
         my_list = list(my_string)
+        my_string = ""
         for i in my_list:
-            if i in "cC":
-                my_list.remove(i)
-                my_string = "".join(my_list)
+            if i not in "cC":
+                my_string += i
     return my_string
