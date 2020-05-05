faadc56062f71a19ce066a75b3a9f02647c90833
Diego Gómez <dagomez2530@gmail.com>
2020-05-05 20:40:42 +0000

    Deleting file

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-def no_c(my_string):
-    if my_string:
-        my_list = list(my_string)
-        my_string = ""
-        for i in my_list:
-            if i not in "cC":
-                my_string += i
-    return my_string
