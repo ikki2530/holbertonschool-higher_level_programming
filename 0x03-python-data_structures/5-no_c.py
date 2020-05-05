9793971e7112533761769fd4d39590af68dfab2d
Diego Gómez <dagomez2530@gmail.com>
2020-05-05 22:17:53 +0000

    Adding file for 5th task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = ""
+    if my_string:
+        for ch in my_string:
+            if ch not in "cC":
+                new += ch
+        return new
