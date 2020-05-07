a21518f6f3b1a59bbc743e32990063aa0e830a34
Diego Gómez <dagomez2530@gmail.com>
2020-05-07 22:38:21 +0000

    Adding files for 12th task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     r_nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = 0
     minn = 0
-    if roman_string or (roman_string is None):
+    if roman_string and  (roman_string is not None):
         for rom in roman_string:
             if rom not in "IVXLCDM":
                 return 0
