9c661ac44181d043daf9aeefe58c64a413ca2425
Diego Gómez <dagomez2530@gmail.com>
2020-05-18 21:36:37 +0000

    Adding files for 7th task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except ValueError as ev:
         print("Exception: {}".format(ev), file=sys.stderr)
+        return False
+    except TypeError as tp:
+        print("Exception: {}".format(tp), file=sys.stderr)
         return False
