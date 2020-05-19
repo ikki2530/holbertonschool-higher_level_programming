b7e1d5648968d7b0d87c06b4517f613e4c131e19
Diego Gómez <dagomez2530@gmail.com>
2020-05-19 21:20:12 +0000

    Adding files for task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """The summary line for a class docstring should fit on one line.
-    
     Attributes:
     """
-    def __init__(self):
-        """Example of docstring on the __init__ method.
-
-        The __init__ method may be documented in either the class level
-        docstring, or as a docstring on the __init__ method itself.
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-        """
-        pass
+    pass
