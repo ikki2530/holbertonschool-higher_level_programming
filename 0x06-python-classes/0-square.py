5c475503a01b70873c65313493f9ea3d11405679
Diego Gómez <dagomez2530@gmail.com>
2020-05-19 21:15:16 +0000

    Adding files for 0 task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 class Square:
     """The summary line for a class docstring should fit on one line.
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
-
+    
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-
     """
-    pass
+    def __init__(self):
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        """
+        pass
