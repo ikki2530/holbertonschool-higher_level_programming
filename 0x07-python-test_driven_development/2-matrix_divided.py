87a1697691980b0e47a91572d676a455d0238703
Diego Gómez <dagomez2530@gmail.com>
2020-05-22 21:35:40 +0000

    Adding files for 2nd point

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
+"""Matrix_divide"""
+
 
 def matrix_divided(matrix, div):
-    """[summary]
-
-    Arguments:
-        matrix {[type]} -- [description]
-        div {[type]} -- [description]
-
-    Raises:
-        TypeError: [description]
-        ZeroDivisionError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-        TypeError: [description]
-
-    Returns:
-        [type] -- [description]
-    """
     new = []
     len_row = 0
+    must_matrix = "matrix must be a matrix (list of lists) of integers/floats"
+    row_size = "Each row of the matrix must have the same size"
     if matrix and type(matrix) == list:
         len_row = len(matrix[0])
         if type(div) not in (int, float):
@@ -28,16 +15,18 @@ def matrix_divided(matrix, div):
             raise ZeroDivisionError("division by zero")
         for row in matrix:
             if type(row) != list:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(must_matrix)
             if len(row) == len_row:
                 len_row = len(row)
+                if len_row == 0:
+                    raise TypeError(must_matrix)
                 for i in row:
                     if type(i) not in (int, float):
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                        raise TypeError(must_matrix)
             else:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError(row_size)
 
         new = [[round(num/div, 2) for num in r] for r in matrix]
         return new
 
-    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    raise TypeError(must_matrix)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -6,3 +6,4 @@ Those are the files that you can find into this projects
 | **0-add_integer.py** | adds 2 integers. |
 | **2-matrix_divided.py** | divides all elements of a matrix. |
 | **3-say_my_name.py** | prints My name is <first name> <last name> |
+
