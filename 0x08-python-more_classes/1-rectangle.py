6b8ceb12e610fcbde4524aa4351111ef98877956
Diego Gómez <dagomez2530@gmail.com>
2020-05-25 23:22:28 +0000

    Adding comments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,19 @@
 
 class Rectangle:
     """class Rectangle"""
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
+        """__init__ initialize the objects
+
+        Arguments:
+            width {int} -- width of the rectangle
+            height {int} -- height of the rectangle
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width < 0:
@@ -19,10 +31,24 @@ class Rectangle:
 
     @property
     def width(self):
+        """ Get the widht
+
+        Returns:
+            int -- Width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width of the rectangle
+
+        Arguments:
+            value {int} -- new value of the width
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -31,10 +57,24 @@ class Rectangle:
 
     @property
     def height(self):
+        """get the height
+
+        Returns:
+            int -- height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height of the rectangle
+
+        Arguments:
+            value {int} -- new value of the height
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
