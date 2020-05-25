7cba29875f5187ee0ce2d0b4eda062e9adb63b03
Diego Gómez <dagomez2530@gmail.com>
2020-05-25 22:16:55 +0000

    Adding files for task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """__init__ initialize the objects
+
+        Arguments:
+            width {int} -- width of the rectangle
+            height {int} -- height of the rectangle
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width of the rectangle
+
+        Arguments:
+            value {int} -- new value of the width
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height of the rectangle
+
+        Arguments:
+            value {int} -- new value of the height
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area of the rectangle
+
+        Returns:
+            [int] -- the area of the rectangle
+        """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """perimeter
+
+        Returns:
+            int -- perimeter of the rectangle
+        """
+        return 2*(self.__width + self.__height)
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -2,4 +2,7 @@
 # Files
 Those are the files that you can find into this projects
 | File | Features |
+| ---- | ---- |
 | **0-rectangle.py** | empty class Rectangle that defines a rectangle |
+| **1-rectangle.py** | class Rectangle that defines a rectangle, with private properties width and height |
+| **2-rectangle.py** | Calculates Area and perimeter |
