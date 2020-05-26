89387dd57371e090455df8f296189ea0ffcbc130
Diego Gómez <dagomez2530@gmail.com>
2020-05-26 20:24:10 +0000

    Adding file for task 19th

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def copy_list(l):
+    """creates a copy of a list
+
+    Arguments:
+        l {list} -- list to be copied
+
+    Returns:
+        list -- new list
+    """
+    new = l[:]
+    return new
