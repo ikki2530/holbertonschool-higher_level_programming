40af91de0da6e79ef15008c89f2657da0be53d0c
Diego Gómez <dagomez2530@gmail.com>
2020-05-27 01:07:37 +0000

    Deleting comments for 19th file

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-"""function to copy a string"""
-
-
 def copy_list(l):
-    """creates a copy of a list
-
-    Arguments:
-        l {list} -- list to be copied
-
-    Returns:
-        list -- new list
-    """
-    new = l[:]
-    return new
+    return l[:]
