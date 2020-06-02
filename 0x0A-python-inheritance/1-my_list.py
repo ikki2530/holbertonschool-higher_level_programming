31a25628c81e429efe586b869a350d9ecf3871f4
Diego Gómez <dagomez2530@gmail.com>
2020-06-02 23:10:12 +0000

    Adding some test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,24 +9,8 @@ class MyList(list):
         list {list} -- list inheritance
     """
 
-    def __init__(self):
-        """Initialize the list
-        """
-        list.__init__(self)
-
     def print_sorted(self):
         """Method for print a list sroted
         """
         new = sorted(self)
         print(new)
-        # num = -1000
-        # temp = 0
-        # new = self[:]
-        # for i in range(len(new)):
-        #     print(num)
-        #     if new[i] < num:
-        #         temp = num
-        #         new[i - 1] = new[i]
-        #         new[i] = temp
-        #     num = new[i]
-        # print(new)
