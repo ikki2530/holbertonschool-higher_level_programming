6d9524a0a629ec1d07010491e52c5d528d7d081c
Diego Gómez <dagomez2530@gmail.com>
2020-06-02 19:10:52 +0000

    Adding files for task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object
+
+    Arguments:
+        obj {any type} -- Can be an object of any type
+
+    Returns:
+        list -- list of available attributes and methods of an object
+    """
+    new = dir(obj)
+    return new
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,6 @@
+# 0x0A. Python - Inheritance
+# Files
+Those are the files that you can find into this projects
+| File | Features |
+| ---- | ---- |
+| **0-lookup.py** | returns the list of available attributes and methods of an object |
