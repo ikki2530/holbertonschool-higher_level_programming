a16712f2ae59e41d9afb3bd0d873d72a7331fcab
Diego Gómez <dagomez2530@gmail.com>
2020-06-02 20:39:41 +0000

    Adding files for task 5 and 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class for geometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
