9b380d31c0d941d710f4773d22f0719879ae4430
Diego Gómez <dagomez2530@gmail.com>
2020-06-03 23:22:16 +0000

    Adding files for task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initialize the class
+
+        Arguments:
+            first_name {str} -- first name
+            last_name {str} -- last name
+            age {int} -- age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """convert to dict
+
+        Returns:
+            dict -- information about the object
+        """
+        return self.__dict__
