77b92150c8586110739d703d4c106e491674548f
Diego Gómez <dagomez2530@gmail.com>
2020-06-03 17:06:08 +0000

    Adding files for task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""read_file function"""
+
+
+def read_file(filename=""):
+    """read a file
+
+    Keyword Arguments:
+        filename {str} -- file name to be readed (default: {""})
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        pass
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,6 @@
+# 0x0A. Python - Inheritance
+# Files
+Those are the files that you can find into this projects
+| File | Features |
+| ---- | ---- |
+| **0-read_file.py** | function that reads a text file (UTF8) and prints it to stdout |
