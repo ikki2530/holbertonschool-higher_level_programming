85fd6e0677d51bca16e129c1e886e8d76f9e95f4
Diego Gomez <dagomez2530@gmail.com>
2020-07-14 21:53:45 +0000

    Adding file for task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- Results must be sorted in ascending order by cities.id
+SELECT * FROM cities WHERE state_id = 1 ORDER BY cities.id ASC;
