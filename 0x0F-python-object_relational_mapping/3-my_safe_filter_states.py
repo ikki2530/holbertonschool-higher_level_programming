7193c79928aa496de60d47ae3d0209e16f2e6a0a
Diego Gomez <dagomez2530@gmail.com>
2020-08-07 21:37:53 +0000

    Adding file for task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""COnnect to the database and executing a query"""
+"""Connect to the database and executing a query"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    """Connect to local database and executes a query"""
     lh = "localhost"
     u = sys.argv[1]
     p = sys.argv[2]
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     db_conn = MySQLdb.connect(host=lh, user=u, passwd=p, db=d, port=3306)
 
     cur = db_conn.cursor()
-    q = "SELECT * FROM states WHERE name = \"{}\"ORDER BY states.id".format(
+    q = "SELECT * FROM states WHERE name=\"{}\" ORDER BY states.id".format(
         nam)
     cur.execute(q)
 
