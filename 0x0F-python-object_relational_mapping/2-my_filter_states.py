49cf63ef2f121f38d13574c681944ef59aedcbe4
Diego Gomez <dagomez2530@gmail.com>
2020-08-07 17:11:15 +0000

    Adding file for task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""COnnect to the database and executing a query"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    lh = "localhost"
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    nam = sys.argv[4]
+    db_conn = MySQLdb.connect(host=lh, user=u, passwd=p, db=d, port=3306)
+
+    cur = db_conn.cursor()
+    q = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id".format(nam)
+    cur.execute(q)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db_conn.close()
