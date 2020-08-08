6d4f584d8fc9c0e10e4da8487196c38041aaa530
Diego Gomez <dagomez2530@gmail.com>
2020-08-08 22:47:05 +0000

    Adding fiel for task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+    lh = "localhost"
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
+            u, p, lh, d))
+    Session = sessionmaker(bind=engine)
+    # Adding Louisiana
+    session = Session()
+    session.add(State(name = 'Louisiana'))
+    session.commit()
