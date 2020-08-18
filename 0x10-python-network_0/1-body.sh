c3b643638eb74f3b699f4947bba1770e80a915b6
Diego Gomez <dagomez2530@gmail.com>
2020-08-18 18:22:54 +0000

    Adding file for task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -w "%{response_code}\n" -s -o /dev/null -X GET "$1")
+response=$(curl -w "%{http_code}\n" -s -o /dev/null -X GET "$1")
 echo "$response"
 if [ "$response" -eq 200 ]; then
-    echo "display the body"
+    
 else
     echo "dont display the body"
 fi
