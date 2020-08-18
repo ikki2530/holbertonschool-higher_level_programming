a3e62c05b3f65684f746cbe2b2baf68120f6d610
Diego Gomez <dagomez2530@gmail.com>
2020-08-18 21:31:08 +0000

    Adding file for task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -d "X-HolbertonSchool-User-Id=98" "$1"
+curl -ds "X-HolbertonSchool-User-Id=98" -X GET "$1"
