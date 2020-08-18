ff7af6d386271c1cc15ec5789a354f6ddfc87d86
Diego Gomez <dagomez2530@gmail.com>
2020-08-18 18:57:12 +0000

    Adding file for task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -w "%{http_code}\n" -s -o /dev/null -X GET -L "$1")
+response=$(curl -w "%{http_code}" -s -o /dev/null -X GET -L "$1")
 
 if [ "$response" == "200" ]; then
     curl -s -X GET -L "$1"
