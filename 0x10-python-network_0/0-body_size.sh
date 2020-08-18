8cfd11065615dff360f1aeb08e3c6705661daa05
Diego Gomez <dagomez2530@gmail.com>
2020-08-18 17:56:22 +0000

    fixing

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -w "%{size_download}\n" --silent --output /dev/null "$1"
+curl -w "%{size_download}\n" -s -o /dev/null "$1"
