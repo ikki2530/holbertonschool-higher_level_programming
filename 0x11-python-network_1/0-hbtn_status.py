3b6e68eee72205596d2542bbaa7e47d0c666b885
Diego Gómez <dagomez2530@gmail.com>
2020-08-24 14:45:35 +0000

    Adding tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import urllib
+"""script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print(type(response))
-    print(html)
-    print(html.decode('UTF-8'))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('UTF-8')))
