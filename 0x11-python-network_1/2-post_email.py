13de6d446acd55d9a4bd32f64a27aaa0f02fdb34
Diego Gómez <dagomez2530@gmail.com>
2020-08-24 16:13:13 +0000

    Adding file for task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,29 +8,12 @@ from urllib.parse import urlencode
 from urllib.request import urlopen, Request
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     url_arg = sys.argv[1]
     email = sys.argv[2]
 
     values = {'email': email}
     data = urlencode(values).encode('utf-8')
     req = Request(url_arg, data)
-    with urlopen(req) as reponse:
+    with urlopen(req) as response:
         print(response.read().decode('utf-8'))
-
-# data = {}
-# data['email'] = email
-# url_values = urllib.parse.urlencode(data)
-# full_url = url_arg + "?" +url_values
-# response = urllib.request.urlopen(full_url)
-# print(response)
-
-# import sys
-# from urllib.parse import urlencode
-# from urllib.request import Request, urlopen
-
-# if __name__ == "__main__":
-#     data = urlencode({'email': sys.argv[2]})
-#     reque = Request(sys.argv[1], data.encode('utf-8'))
-#     with urlopen(reque) as re:
-#         print(re.read().decode('utf-8'))
