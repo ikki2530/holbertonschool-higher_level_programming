f9616a06479e2cc02eb081818626651364b39e5c
Diego Gomez <dagomez2530@gmail.com>
2020-08-24 17:08:24 +0000

    Adding file for task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url_arg = sys.argv[1]
+    response = requests.get("https://intranet.hbtn.io/status")
+    print(response.header)
