1f7743d989ebfa44c04ffa1bd9e85e5161e5a394
Diego Gomez <dagomez2530@gmail.com>
2020-08-24 16:11:16 +0000

    Adding file for task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,24 +3,20 @@
 script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter,
 """
-# import sys
-# import urllib.request
-# from urllib.parse import urlencode
-# from urllib.request import urlopen, Request
+import sys
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
 
-# if __name__=="__main__":
-#     url_arg = sys.argv[1]
-#     email = sys.argv[2]
 
-#     values = {'email': email}
-#     data1 = urlencode(values)
-#     data = data1.encode('utf-8')
-#     req = Request(url_arg, data)
-#     print(111111111111111)
-#     with urlopen(req) as reponse:
-#         print(22222222222222222)
-#         the_page = reponse.read()
-#         print(the_page)
+if __name__=="__main__":
+    url_arg = sys.argv[1]
+    email = sys.argv[2]
+
+    values = {'email': email}
+    data = urlencode(values).encode('utf-8')
+    req = Request(url_arg, data)
+    with urlopen(req) as reponse:
+        print(response.read().decode('utf-8'))
 
 # data = {}
 # data['email'] = email
@@ -29,12 +25,12 @@ to the passed URL with the email as a parameter,
 # response = urllib.request.urlopen(full_url)
 # print(response)
 
-import sys
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+# import sys
+# from urllib.parse import urlencode
+# from urllib.request import Request, urlopen
 
-if __name__ == "__main__":
-    data = urlencode({'email': sys.argv[2]})
-    reque = Request(sys.argv[1], data.encode('utf-8'))
-    with urlopen(reque) as re:
-        print(re.read().decode('utf-8'))
+# if __name__ == "__main__":
+#     data = urlencode({'email': sys.argv[2]})
+#     reque = Request(sys.argv[1], data.encode('utf-8'))
+#     with urlopen(reque) as re:
+#         print(re.read().decode('utf-8'))
