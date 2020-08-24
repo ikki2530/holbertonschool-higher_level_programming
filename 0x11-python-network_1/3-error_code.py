5b4c6096b9a77d0f53f0db257ff297bb76761931
Diego Gómez <dagomez2530@gmail.com>
2020-08-24 16:40:00 +0000

    Adding file for task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL and 
+script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 """
 import sys
 from urllib.parse import urlencode
 from urllib.request import urlopen, Request
+from urllib.error import URLError
 
 
 if __name__ == "__main__":
     url_arg = sys.argv[1]
     req = Request(url_arg)
+
     try:
         with urlopen(req) as response:
-            print(response.read())
-    except urllib.error.URLError as e:
-        print(e.reason)
+            print(response.read().decode('utf-8'))
+    except URLError as e:
+        print(e.code)
