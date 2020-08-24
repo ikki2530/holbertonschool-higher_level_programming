63f70a95188e58ddb66856997e628a866fa2a5e3
Diego Gómez <dagomez2530@gmail.com>
2020-08-24 22:36:14 +0000

    Checking pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,5 +9,5 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/users/{}".format(sys.argv[1])
-    response = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2])) 
+    response = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
     print(response.json().get('id'))
