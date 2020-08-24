7c3062e0cef55dfd8d3d671b265160cf76796721
Diego Gómez <dagomez2530@gmail.com>
2020-08-24 20:07:28 +0000

    Adding file for task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,8 +7,21 @@ import sys
 
 
 if __name__ == "__main__":
-    q = sys.argv[1]
-    response requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-    print(response.headers)
-    print("-------------------------")
-    print(response.body)
+    lg = len(sys.argv)
+    if lg < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    response = requests.post(
+        'http://cb6c4d06c482.22216bed.hbtn-cod.io:5000/search_user',
+        data={'q': q})
+
+    dic = response.json()
+    if response.headers['content-type'] == 'application/json' and dic:
+        print("[{}] {}".format(dic['id'], dic['name']))
+    else:
+        if response.headers['content-type'] != 'application/json':
+            print("Not a valid JSON")
+        elif not response.json():
+            print("No result")
