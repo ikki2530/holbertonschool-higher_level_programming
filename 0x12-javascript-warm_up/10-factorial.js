be80a99d386615e29537b778038bf3dd69d02b94
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 20:01:47 +0000

    Adding file for task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,14 +3,14 @@
 const myArgs = process.argv.slice(2);
 
 function factorial (num) {
-    if (isNaN(num) || num == 0) {
-        return 1;
-    }
-    else {
-        num = num * factorial(num - 1);
-    }
-    return num;
+  if (isNaN(num) || num === 0) {
+    return 1;
+  } else {
+    num = num * factorial(num - 1);
+  }
+  return num;
 }
+
 const number = parseInt(myArgs[0]);
 const result = factorial(number);
 console.log(result);
