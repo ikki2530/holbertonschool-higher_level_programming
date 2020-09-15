252fc3130410d4ef368968115cd386f82ad062a8
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 21:38:26 +0000

    Adding file for task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,21 +5,18 @@ const myArgs = process.argv.slice(2);
 if (myArgs.length === 0 || myArgs.length === 1) {
   console.log(0);
 } else {
+  const arr = [];
   for (let i = 0; i < myArgs.length; i++) {
-    myArgs[i] = parseInt(myArgs[i]);
+    arr.push(parseInt(myArgs[i], 10));
   }
+  arr.sort((a, b) => a - b);
+  arr.reverse();
 
-  let mx = myArgs[0];
-  let secondMx = myArgs[0];
-  for (let j = 1; j < myArgs.length; j++) {
-    if (myArgs[j] > mx) {
-      secondMx = mx;
-      mx = myArgs[j];
-    } else if (myArgs[j] > secondMx) {
-      if (myArgs[j] !== mx) {
-        secondMx = myArgs[j];
-      }
+  const mx = arr[0];
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] < mx) {
+      console.log(arr[j]);
+      break;
     }
   }
-  console.log(secondMx);
 }
