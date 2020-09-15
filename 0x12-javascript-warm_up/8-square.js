5ab6a34944055a0ae3c259c717e88f554b31add9
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 19:26:14 +0000

    Adding file for task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // prints a square
 const myArgs = process.argv.slice(2);
-let square = ""
+let square = '';
 if (isNaN(myArgs[0])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(myArgs[0]); i++) {
     for (let j = 0; j < parseInt(myArgs[0]); j++) {
-      square += "X";
+      square += 'X';
     }
-    console.log(square)
-    square = ""
+    console.log(square);
+    square = '';
   }
 }
