458e6024d0b565b8ccd32ababb5c45b1325c66b2
Diego Gomez <dagomez2530@gmail.com>
2020-09-15 19:05:08 +0000

    Adding file for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints 3 lines using an array of string and a loop
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing']
+
+for (let i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+}
