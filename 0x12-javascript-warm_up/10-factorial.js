70edd50baa86814fea37e084c98430788038292f
Diego Gomez <dagomez2530@gmail.com>
2020-09-15 19:58:03 +0000

    Adding file for task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints the addition of 2 integers
+const myArgs = process.argv.slice(2);
+
+function factorial (num) {
+    if (isNaN(num) || num == 0) {
+        return 1;
+    }
+    else {
+        num = num * factorial(num - 1);
+    }
+    return num;
+}
+const number = parseInt(myArgs[0]);
+const result = factorial(number);
+console.log(result);
