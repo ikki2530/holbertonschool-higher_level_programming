b1fc736bdc10fb1037bc783d6eff10379ea24830
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 17:12:46 +0000

    Adding file for task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 // prints two arguments passed to it, in the following format: “ is ”
 const myArgs = process.argv.slice(2);
 
-console.log(myArgs[0] + " is " + myArgs[1]);
+console.log(myArgs[0] + ' is ' + myArgs[1]);
