10e1af634fe40f419276249848d2ab104550c3a1
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 19:14:47 +0000

    Adding file for task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const myArgs = process.argv.slice(2);
 if (isNaN(myArgs[0])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(myArgs[0]); i++){
-    console.log("C is fun")
+  for (let i = 0; i < parseInt(myArgs[0]); i++) {
+    console.log('C is fun');
   }
 }
