d3528f384cc929cbb703234e9ac3d4f45f7708d3
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 20:53:20 +0000

    Adding file for task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,22 +2,24 @@
 // prints the addition of 2 integers
 const myArgs = process.argv.slice(2);
 
-if (myArgs.length === 0 || myArgs.length == 1) {
-    console.log(0)
+if (myArgs.length === 0 || myArgs.length === 1) {
+  console.log(0);
 } else {
   for (let i = 0; i < myArgs.length; i++) {
     myArgs[i] = parseInt(myArgs[i]);
   }
 
   let mx = myArgs[0];
-  let second_mx = myArgs[0];
+  let secondMx = myArgs[0];
   for (let j = 1; j < myArgs.length; j++) {
     if (myArgs[j] > mx) {
-      second_mx = mx;
+      secondMx = mx;
       mx = myArgs[j];
-    } else if (myArgs[j] > second_mx) {
-      second_mx = myArgs[j];
+    } else if (myArgs[j] > secondMx) {
+      if (myArgs[j] !== mx) {
+        secondMx = myArgs[j];
+      }
     }
   }
-  console.log(second_mx);
+  console.log(secondMx);
 }
