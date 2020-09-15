0bd4cc62b48c08e2de86b6cf2cc69919bfcebb1f
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 17:32:14 +0000

    Fixing semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 const myArgs = process.argv.slice(2);
 if (isNaN(myArgs[0])) {
-    console.log('Not a number')
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(myArgs[0]))
+  console.log('My number: ' + parseInt(myArgs[0]));
 }
