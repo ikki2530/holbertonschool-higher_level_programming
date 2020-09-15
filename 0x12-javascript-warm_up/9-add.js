f7501d8c1cf97c8273964b4bf089d7598bd6f74e
Diego Gómez <dagomez2530@gmail.com>
2020-09-15 19:36:37 +0000

    Adding file for task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 // prints the addition of 2 integers
 const myArgs = process.argv.slice(2);
 
-function add(a, b) {
-    console.log(parseInt(a) + parseInt(b));
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
 }
 
-add(myArgs[0], myArgs[1])
+add(myArgs[0], myArgs[1]);
