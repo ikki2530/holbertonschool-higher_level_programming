8c85f9580cb79fd1790f6a82b3ea44c7a165f5b4
Diego Gómez <dagomez2530@gmail.com>
2020-09-21 21:25:06 +0000

    Adding file for task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,11 +2,11 @@
 /* returns the reversed version of a list */
 
 exports.esrever = function (list) {
-  let rev = [];
+  const rev = [];
   const lg = list.length;
   for (let i = lg - 1; i >= 0; i--) {
     rev.push(list[i]);
   }
 
   return rev;
-}
+};
