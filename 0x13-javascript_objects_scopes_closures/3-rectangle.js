cd011527d370ae8154cd3519f2e43b2da01a185b
Diego Gomez <dagomez2530@gmail.com>
2020-09-21 19:48:25 +0000

    Adding file for task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/* Empty class Rectangle */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let rect = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rect += 'X';
+      }
+      console.log(rect);
+      rect = '';
+    }
+  }
+}
+
+module.exports = Rectangle;
