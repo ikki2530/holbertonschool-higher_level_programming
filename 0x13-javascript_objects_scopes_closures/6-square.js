6e1b7723705c01a3d717dd6b22964c214730c4e8
Diego Gomez <dagomez2530@gmail.com>
2020-09-21 20:50:12 +0000

    Adding file for task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* class Square inherits */
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      super.print()
+    } else {
+      let ch = '';
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.width; j++){
+          ch += c;
+        }
+        console.log(ch);
+        ch = '';
+      }
+    }
+  }
+}
+
+module.exports = Square;
