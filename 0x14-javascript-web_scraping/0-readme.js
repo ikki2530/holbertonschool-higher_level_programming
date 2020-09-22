a2149dd3367b54de8e81648729c37d23a09e5106
Diego Gómez <dagomez2530@gmail.com>
2020-09-22 16:25:35 +0000

    fixing semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 /* reads and prints the content of a file. */
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
-fs.readFile(myArgs[0], 'utf-8', function (err, data) { 
-  if (err){
+fs.readFile(myArgs[0], 'utf-8', function (err, data) {
+  if (err) {
     return console.log(err);
   }
   console.log(data);
-})
+});
