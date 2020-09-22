7b56bf2624403533de83879fd5167b0012fd1b14
Diego Gómez <dagomez2530@gmail.com>
2020-09-22 17:55:06 +0000

    Adding file for task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,23 @@
 const request = require('request');
 const myArgs = process.argv.slice(2);
 
-const find_id = '18';
-const urlId = myArgs[0] + '/18';
-request.get(urlId, (err, res, body) => {
+const findId = '18';
+const url = myArgs[0];
+let cont = 0;
+request.get(url, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  console.log(JSON.parse(body).title);
+  const listDict = JSON.parse(body).results;
+  for (let i = 0; i < listDict.length; i++) {
+    // console.log(listDict[i].characters);
+    for (let j = 0; j < listDict[i].characters.length; j++) {
+      // check if in each url is id 18
+      if (listDict[i].characters[j].includes(findId)) {
+        cont += 1;
+      }
+    }
+  }
+  console.log(cont);
+  // console.log(JSON.parse(body).results[0]);
 });
