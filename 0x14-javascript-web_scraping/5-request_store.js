fd173ab6bf67575497d11d797c20ebd301916ed7
Diego Gómez <dagomez2530@gmail.com>
2020-09-22 20:13:35 +0000

    Adding file for task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* gets the contents of a webpage and stores it in a file. */
 const request = require('request');
+const fs = require('fs');
 const myArgs = process.argv.slice(2);
 
 const url = myArgs[0];
@@ -10,6 +11,10 @@ request.get(url, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  const listDict = JSON.parse(body).results;
-  console.log(listDict);
+  console.log(body);
+  fs.writeFile(file, body, 'utf-8', function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
