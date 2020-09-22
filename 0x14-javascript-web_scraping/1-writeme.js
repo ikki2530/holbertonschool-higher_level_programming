eef4e26f31b806e8942dd15ec755b5683ef24dca
Diego Gomez <dagomez2530@gmail.com>
2020-09-22 16:31:52 +0000

    Adding file for task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,8 @@
 /* reads and prints the content of a file. */
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
-
-try {
-  const file = myArgs[0];
-  const cont = myArgs[1];
-  fs.writeFileSync(file, cont);
-} catch (err) {
-  console.log(err);
-}
+fs.writeFile(myArgs[0], myArgs[1], function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
