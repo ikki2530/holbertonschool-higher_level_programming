701edbad6132b883f31c4cabed2dbe967d6b943d
Diego Gomez <dagomez2530@gmail.com>
2020-09-22 16:23:31 +0000

    Fixing error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,33 +2,9 @@
 /* reads and prints the content of a file. */
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
-
-
-  
-// fs.readFile(myArgs[0], (err, data) => {
-//   try {
-//     console.log(data.toString()); 
-//   } catch (err) {
-//     console.log(err);
-//   }
-
-// }) 
-
-try {
-  const dataA = fs.readFileSync(myArgs[0], 'utf8');
-  console.log(dataA);
-}
-catch (err) {
-  console.log(err);
-}
-
-// try {
-//   fs.readFile(myArgs[0], (err, data) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log(data.toString());
-//   })
-// } catch (ex) {
-//   console.log(ex)
-// }
+fs.readFile(myArgs[0], 'utf-8', function (err, data) { 
+  if (err){
+    return console.log(err);
+  }
+  console.log(data);
+})
