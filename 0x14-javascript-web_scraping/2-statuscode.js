6a3fc26f9770aa24ee74095b2577f6e77c4d6887
Diego Gómez <dagomez2530@gmail.com>
2020-09-22 16:47:22 +0000

    Adding file for task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const request = require('request');
 const myArgs = process.argv.slice(2);
 
 request.get(myArgs[0], (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(JSON.parse(body));
+  if (err) {
+    return console.log(err);
+  }
+  console.log(res.statusCode);
 });
