bcb23e37c0040db18f9608aa1c3209c2c1a82c4d
Diego Gómez <dagomez2530@gmail.com>
2020-09-24 21:18:59 +0000

    Adding comments

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
 // fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello.
 $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-  $('DIV#hello').text(data.hello)
+  $('DIV#hello').text(data.hello);
 });
