f74852107aac5b3f3da1204645f754f29c405fda
Diego Gómez <dagomez2530@gmail.com>
2020-09-24 21:46:40 +0000

    Fixing semistandard

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,5 +1,5 @@
 // updates the text color of the HTML tag HEADER to red (#FF0000)
-window.addEventListener("DOMContentLoaded", function(){
-    // Your code
-    document.querySelector('HEADER').style.color = '#FF0000';
+window.addEventListener('DOMContentLoaded', function () {
+  // Your code
+  document.querySelector('HEADER').style.color = '#FF0000';
 });
