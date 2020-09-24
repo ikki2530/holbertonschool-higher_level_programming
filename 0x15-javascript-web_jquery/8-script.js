fe0f1d8ff4427586a64037ec3e41b9e04a536448
Diego <dagomez2530@gmail.com>
2020-09-24 16:16:54 -0500

    Adding file for task 8 and 9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+// fetches and lists all movies title by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (i, movie) {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>')
+  });
+  // for (let i = 0; i < data.results.length; i++) {
+  //   $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+  // }
+});
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,4 @@
+// fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello.
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data.hello)
+});
