210a84e962b1c195fcf7ab2857d66e2233308724
Diego <dagomez2530@gmail.com>
2020-09-24 15:15:53 -0500

    Adding file for task

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,4 @@
+// change color, red -> green and green -> red
 $("DIV#toggle_header").click(function() {
-  if ($("HEADER").hasClass("green")) {
-    $("HEADER").removeClass("green");
-    $("HEADER").toggleClass("red");
-  } else {
-    $("HEADER").removeClass("red");
-    $("HEADER").toggleClass("green");
-  }
+  $("HEADER").toggleClass("green red");
 });
